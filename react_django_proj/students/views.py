73f5937ec34f82ec08efd887c890cb17447f9be9
robinho101 <dkdsmg1dh>
2023-03-11 18:22:19 +0300

    react added

--- react_django_proj/students/views.py
+++ react_django_proj/students/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
+from .models import *
+from .serializers import *
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def students_list(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        seializer = StudentSerializer(
+            students, context={'request': request}, many=True)
+        return Response(seializer.data)
+
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            seializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'DELETE'])
+def students_detail(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = StudentSerializer(
+        student, data=request.data, context={'request': request})
+
+    if request.method == 'PUT':
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
